f1a7b368531419ca535cd49ea07d8d3f5c2545eb
Varphone Wong <varphone@qq.com>
2020-05-05 23:12:27 +0800

    Impl convert between HI_BOOL and bool
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,24 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+/// Make HI_BOOL can convert to bool.
+impl std::convert::Into<bool> for HI_BOOL {
+    fn into(self) -> bool {
+        self == HI_BOOL::HI_TRUE
+    }
+}
+
+/// Make bool can convert to HI_BOOL.
+impl std::convert::Into<HI_BOOL> for bool {
+    fn into(self) -> HI_BOOL {
+        if self {
+            HI_BOOL::HI_TRUE
+        } else {
+            HI_BOOL::HI_FALSE
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
