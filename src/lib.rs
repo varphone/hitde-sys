a08bea3ec55b391ff536847f4bfb2b5469ebcc9d
Varphone Wong <varphone@qq.com>
2020-05-06 12:35:47 +0800

    Impl missing traits for some types
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-﻿#![allow(non_upper_case_globals)]
+#![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::unreadable_literal)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
@@ -21,6 +22,78 @@ impl std::convert::Into<HI_BOOL> for bool {
         }
     }
 }
+
+// Fix incomplete PartialEq trait for TDE_COMPOSOR_S
+impl PartialEq for TDE_COMPOSOR_S {
+    fn eq(&self, other: &Self) -> bool {
+        self.stSrcSurface == other.stSrcSurface
+            && self.stInRect == other.stInRect
+            && self.stOutRect == other.stOutRect
+            && self.stOpt == other.stOpt
+    }
+}
+
+// Fix incomplete Eq trait for TDE_COMPOSOR_S
+impl Eq for TDE_COMPOSOR_S {}
+
+// Fix incomplete PartialEq trait for TDE2_COLORKEY_U
+impl PartialEq for TDE2_COLORKEY_U {
+    fn eq(&self, other: &Self) -> bool {
+        unsafe { self._bindgen_union_align == other._bindgen_union_align }
+    }
+}
+
+// Fix incomplete Eq trait for TDE2_COLORKEY_U
+impl Eq for TDE2_COLORKEY_U {}
+
+// Fix incomplete PartialEq trait for TDE2_OPT_S
+impl PartialEq for TDE2_OPT_S {
+    fn eq(&self, other: &Self) -> bool {
+        self.enAluCmd == other.enAluCmd
+            && self.enRopCode_Color == other.enRopCode_Color
+            && self.enRopCode_Alpha == other.enRopCode_Alpha
+            && self.enColorKeyMode == other.enColorKeyMode
+            && self.unColorKeyValue == other.unColorKeyValue
+            && self.enClipMode == other.enClipMode
+            && self.stClipRect == other.stClipRect
+            && self.enDeflickerMode == other.enDeflickerMode
+            && self.enFilterMode == other.enFilterMode
+            && self.enMirror == other.enMirror
+            && self.bClutReload == other.bClutReload
+            && self.u8GlobalAlpha == other.u8GlobalAlpha
+            && self.enOutAlphaFrom == other.enOutAlphaFrom
+            && self.u32Colorize == other.u32Colorize
+            && self.stBlendOpt == other.stBlendOpt
+            && self.stCscOpt == other.stCscOpt
+            && self.bCompress == other.bCompress
+            && self.bDecompress == other.bDecompress
+    }
+}
+
+// Fix incomplete Eq trait for TDE2_OPT_S
+impl Eq for TDE2_OPT_S {}
+
+// Fix incomplete PartialEq trait for TDE2_PATTERN_FILL_OPT_S
+impl PartialEq for TDE2_PATTERN_FILL_OPT_S {
+    fn eq(&self, other: &Self) -> bool {
+        self.enAluCmd == other.enAluCmd
+            && self.enRopCode_Color == other.enRopCode_Color
+            && self.enRopCode_Alpha == other.enRopCode_Alpha
+            && self.enColorKeyMode == other.enColorKeyMode
+            && self.unColorKeyValue == other.unColorKeyValue
+            && self.enClipMode == other.enClipMode
+            && self.stClipRect == other.stClipRect
+            && self.bClutReload == other.bClutReload
+            && self.u8GlobalAlpha == other.u8GlobalAlpha
+            && self.enOutAlphaFrom == other.enOutAlphaFrom
+            && self.u32Colorize == other.u32Colorize
+            && self.stBlendOpt == other.stBlendOpt
+            && self.stCscOpt == other.stCscOpt
+    }
+}
+
+// Fix incomplete Eq trait for TDE2_PATTERN_FILL_OPT_S
+impl Eq for TDE2_PATTERN_FILL_OPT_S {}
 
 #[cfg(test)]
 mod tests {
